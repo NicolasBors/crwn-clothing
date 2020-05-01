231fd17ebada710cb727a25e1f4b48ab69121a87
Nicolas Borson <nicolasborson@hotmail.fr>
2020-05-01 10:54:28 +0200

    fixed bugs in building caused by depreciated webpack and dependencies in package json

--- scripts/start.js
+++ scripts/start.js
@@ -19,7 +19,7 @@ var checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 var formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 var getProcessForPort = require('react-dev-utils/getProcessForPort');
 var openBrowser = require('react-dev-utils/openBrowser');
-var prompt = require('react-dev-utils/prompt');
+var inquirer = require('inquirer');
 var fs = require('fs');
 var config = require('../config/webpack.config.dev');
 var paths = require('../config/paths');
@@ -27,6 +27,8 @@ var paths = require('../config/paths');
 var useYarn = fs.existsSync(paths.yarnLockFile);
 var cli = useYarn ? 'yarn' : 'npm';
 var isInteractive = process.stdout.isTTY;
+
+module.exports = inquirer;
 
 // Warn and crash if required files are missing
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
@@ -314,3 +316,4 @@ detect(DEFAULT_PORT).then(port => {
     console.log(chalk.red('Something is already running on port ' + DEFAULT_PORT + '.'));
   }
 });
+
