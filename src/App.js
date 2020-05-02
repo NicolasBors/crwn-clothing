9809a8fd6bd1b302d775b06daf469906f45d9281
Nicolas Borson <nicolasborson@hotmail.fr>
2020-05-02 13:00:40 +0200

    implement code that pulls in shop data from firestore and stores it into shop reducer from shoppage. included withspinner hox

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
@@ -16,12 +16,13 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selectors'
 
-class App extends Component {
+class App extends React.Component {
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
     const { setCurrentUser } = this.props
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
@@ -69,4 +70,6 @@ const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+  // "start": "serve -s build",
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,15 +13,14 @@ const config = {
     measurementId: "G-KCY8P711R6"
 }
 
+firebase.initializeApp(config)
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;    
+    if (!userAuth) return;
 
     const userRef = firestore.doc(`users/${userAuth.uid}`)
 
-    const snapShot = await userRef.get()    
-
-    console.log(snapShot);
-    
+    const snapShot = await userRef.get()
 
     if (!snapShot.exists) {
         const { displayName, email } = userAuth
@@ -39,12 +38,42 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef
 }
 
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+) => {
+    const collectionRef = firestore.collection(collectionKey)
 
-firebase.initializeApp(config)
+    const batch = firestore.batch()
 
-export const firestore = firebase.firestore()
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc()
+        batch.set(newDocRef, obj)
+    })
+
+    return await batch.commit()
+}
+
+export const convertCollectionSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const {title, items} = doc.data()
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    })
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection
+        return accumulator
+    }, {})
+}
 
 export const auth = firebase.auth()
+export const firestore = firebase.firestore()
 
 const provider = new firebase.auth.GoogleAuthProvider()
 provider.setCustomParameters({ prompt: 'select_account' })
