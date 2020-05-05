7b23919828b63bf5413bbcf222ecf31f00a51b51
Nicolas Borson <nicolasborson@hotmail.fr>
2020-05-05 13:04:57 +0200

    created backend for stripe

---
+++ client/build/precache-manifest.e6fb2c4a8b65669009bb19e20815e1bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b74c3bd3bfe7a91623c52df63241cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed0a0cd1e9703d0716e7",
+    "url": "/static/css/main.676da0bc.chunk.css"
+  },
+  {
+    "revision": "e515d8f767cf74ceab4a",
+    "url": "/static/js/2.6eef487e.chunk.js"
+  },
+  {
+    "revision": "f9416c2fa5dace694260608407c8cf2e",
+    "url": "/static/js/2.6eef487e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0a0cd1e9703d0716e7",
+    "url": "/static/js/main.b761b07e.chunk.js"
+  },
+  {
+    "revision": "9dce950c1f8d5f2c45b4",
+    "url": "/static/js/runtime-main.a4da908e.js"
+  },
+  {
+    "revision": "3589bddbe338e444d408f4dbc545ca1e",
+    "url": "/static/media/OpenSansCondensed-Light.3589bddb.ttf"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
